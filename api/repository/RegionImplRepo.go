e006a92302988789fe041d0b7f64a0e96d42bcbe
aditya37 <aditrahman212@gmail.com>
2020-09-02 13:40:05 +0700

    [Refactor][0] Change model database for GORM and connect to db with Gorm [Oleh: @aditya37]

--- api/repository/RegionImplRepo.go
+++ api/repository/RegionImplRepo.go
@@ -8,120 +8,53 @@
 package repository
 
 import (
-	"database/sql"
-
 	model "github.com/aditya37/backend-jobs/api/model/Entity"
+	"gorm.io/gorm"
 )
 
 type RegionImpl struct {
-	Psql *sql.DB
+	database *gorm.DB
 }
 
-func NewRegionImpl(psqlClient *sql.DB) IRegionRepo {
-	return &RegionImpl{Psql:psqlClient}
+func NewRegionImpl(dbClient *gorm.DB) IRegionRepo {
+	return &RegionImpl{database:dbClient}
 }
 
 // Add District
-func (p *RegionImpl) AddDistrict(district *model.District) (*model.District,error){
-	begin,err := p.Psql.Begin()
-	if err != nil {
-		return nil,err
-	}
-	_,err = begin.Exec("INSERT INTO tbl_region VALUES($1,$2)",&district.IdDistrict,&district.DistrictName)
-	if err != nil {
-		return nil,err
-	}
-	begin.Commit()
-	return district,err
+func (p *RegionImpl) AddDistrict(district *model.District) *model.District {
+	p.database.Save(&district)
+	return district
 }
 
 // Add Country
-func (p *RegionImpl) AddCountry(country *model.Country) (*model.Country,error){
-	begin,err := p.Psql.Begin()
-	if err != nil {
-		return nil,err
-	}
-	_,err = begin.Exec("INSERT INTO tbl_country VALUES($1,$2)",&country.IdCountry,&country.CountryName)
-	if err != nil {
-		return nil,err
-	}
-	begin.Commit()
-	return country,err
+func (p *RegionImpl) AddCountry(country *model.Country) *model.Country {
+	p.database.Save(&country)
+	return country
 }
 
 // Add Province
-func (p *RegionImpl) AddProvince(province *model.Province) (*model.Province,error){
-	begin,err := p.Psql.Begin()
-	if err != nil {
-		return nil,err
-	}
-	_,err = begin.Exec("INSERT INTO tbl_province VALUES($1,$2)",&province.IdProvince,&province.ProvinceName)
-	if err != nil {
-		return nil,err
-	}
-	begin.Commit()
-	return province,err
+func (p *RegionImpl) AddProvince(province *model.Province) *model.Province {
+	p.database.Save(&province)
+	return province
 }
 
 // Get District 
-func (p *RegionImpl) GetDistrict() ([]model.District,error){
-	var (
-		districts model.District
-		arrDistrict []model.District
-	)
-	
-	result,err := p.Psql.Query("SELECT * FROM tbl_district")
-	if err != nil {
-		return nil,err
-	}
-
-	for result.Next() {
-		if err := result.Scan(&districts.IdDistrict,&districts.DistrictName); err != nil {
-			return nil, err
-		}
-		arrDistrict = append(arrDistrict,districts)
-	}
-	return arrDistrict,nil
+func (p *RegionImpl) GetDistrict() []model.District {
+	var districts []model.District
+	p.database.Find(&districts)
+	return districts
 }
 
 //  Get Country
-func (p *RegionImpl) GetCountry() ([]model.Country,error) {
-	var (
-		countrys model.Country
-		arrCountry []model.Country
-	)
-
-	result,err := p.Psql.Query("SELECT * FROM tbl_country")
-	if err != nil {
-		return nil,err
-	}
-
-	for result.Next() {
-		if err := result.Scan(&countrys.IdCountry,&countrys.CountryName); err != nil {
-			return nil,err
-		}
-		arrCountry = append(arrCountry,countrys)
-	}
-	return arrCountry,nil
+func (p *RegionImpl) GetCountry() []model.Country {
+	var countrys []model.Country
+	p.database.Find(&countrys)
+	return countrys
 }
 
 // Get Province
-func (p *RegionImpl) GetProvince()	([]model.Province,error) {
-	var (
-		provinces   model.Province
-		arrProvince []model.Province
-	)
-
-	result,err := p.Psql.Query("SELECT * FROM tbl_province")
-	if err != nil {
-		return nil,err
-	}
-
-	for result.Next() {
-		if err := result.Scan(&provinces.IdProvince,&provinces.ProvinceName); err != nil {
-			return nil,err
-		}
-		arrProvince = append(arrProvince,provinces)
-	}
-	return arrProvince,nil
+func (p *RegionImpl) GetProvince() []model.Province {
+	var provinces []model.Province
+	p.database.Find(&provinces)
+	return provinces
 }
