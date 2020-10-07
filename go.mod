610eb9f8f5ce51ed293c29377761c660d8b0da37
aditya37 <aditrahman212@gmail.com>
2020-10-07 11:32:32 +0700

    SPRINT-1

--- go.mod
+++ go.mod
@@ -3,14 +3,22 @@ module github.com/aditya37/backend-jobs
 go 1.15
 
 require (
+	cloud.google.com/go/firestore v1.3.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-redis/redis/v8 v8.1.3
+	github.com/google/go-cmp v0.5.2 // indirect
 	github.com/joho/godotenv v1.3.0
+	github.com/labstack/echo/v4 v4.1.17
 	github.com/onsi/ginkgo v1.14.1
 	github.com/onsi/gomega v1.10.2
 	github.com/segmentio/ksuid v1.0.3
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	golang.org/x/sys v0.0.0-20200909081042-eff7692f9009 // indirect
+	golang.org/x/net v0.0.0-20200925080053-05aa5d4ee321 // indirect
+	golang.org/x/sys v0.0.0-20200923182605-d9f96fdee20d // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/api v0.32.0
+	google.golang.org/protobuf v1.25.0 // indirect
 	gorm.io/driver/postgres v1.0.0
 	gorm.io/gorm v1.20.0
 
--- main.go
+++ main.go
@@ -8,75 +8,52 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"time"
 
-	model "github.com/aditya37/backend-jobs/api/Model/Entity/Employe"
-	"github.com/segmentio/ksuid"
-
-	infra "github.com/aditya37/backend-jobs/api/Infrastructure"
-
+	controller "github.com/aditya37/backend-jobs/api/Controller/Employe"
+	infrastructure "github.com/aditya37/backend-jobs/api/Infrastructure"
 	repository "github.com/aditya37/backend-jobs/api/Repository/Employe"
 	service "github.com/aditya37/backend-jobs/api/Service/Employe"
-
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 )
 
 var (
-	connection infra.DatabaseConnection = infra.NewDatabaseConnection()
-	redisConn infra.IRedisConn = infra.NewRedisConn("localhost:6379","",0,10)
+	router infrastructure.IEchoRouter = infrastructure.NewEchoRouter(echo.New())
+	database infrastructure.DatabaseConnection = infrastructure.NewDatabaseConnection()
+	redisCache infrastructure.IRedisConn = infrastructure.NewRedisConn("localhost:6379","",0,900)
+	firebase infrastructure.IFireStorage = infrastructure.NewFireStorageImpl()
 )
 
 func main() {
-	env := godotenv.Load()
-	if env != nil {
-		log.Panic("Error .env file not found")
-	}
-
-	dbConn,err := connection.DatabaseConn(os.Getenv("DBHOST"),os.Getenv("DBPORT"),os.Getenv("DBUSER"),os.Getenv("DBPASSWORD"),os.Getenv("DBNAME"))
-	if err != nil {
-		fmt.Println("Database connection error ",err)
-	}
-	connection.DatabaseMigrate()
-	EmployeRepo := repository.NewEmployeImpl(dbConn)
-	Empl := service.NewEmployeService(EmployeRepo)
-	
-	account := &model.EmployeAccount{
-		Username: "arifnyik",
-		Password: "lymousin",
-		Email: "aditya.krohman@gmail.com",
-		PhotoProfile: "google.com",
-		DateCreate: time.Now(),
-		DateUpdate: time.Now(),
+	LoadEnv := godotenv.Load()
+	if LoadEnv != nil {
+		log.Fatalln(LoadEnv)
 	}
 	
-	DoRegister,err := Empl.RegisterEmploye(account)
+	ConnectDB,err := database.DatabaseConn(os.Getenv("DBHOST"),os.Getenv("DBPORT"),os.Getenv("DBUSER"),os.Getenv("DBPASSWORD"),os.Getenv("DBNAME"))
 	if err != nil {
-		log.Println(err.Error())
+		log.Panic(err)
 	}
-	id := ksuid.New()
-	err = redisConn.AddEmailVerify(DoRegister.Email,id.String())
-	if err != nil {
-		log.Println(err)
-	}
-	Empl.SendEmailVerify(DoRegister.Email,DoRegister.Username,id.String())
-	redisKey,err := redisConn.VerifyEmail(DoRegister.Email)
-	if err != nil {
-		log.Println(err)
-	}
-	if len(redisKey) == 0 {
-		fmt.Println("Cache Not Found",redisKey)
-	}else{
-		if redisKey == id.String() {
-			EmployeRepo.EmployeEmailVerify(DoRegister.Email)
-		}
-	}
+	database.DatabaseMigrate()
 
-	// DoLogin,err := Empl.EmployeLogin("aditya","lymousin")
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// }
-	// fmt.Println(DoLogin)
+	EmployeRepo 	:= repository.NewEmployeImpl(ConnectDB)
+	EmployeService  := service.NewEmployeService(EmployeRepo)
+	EmployeController := controller.NewEmployeController(EmployeService,redisCache,firebase)
+	
+	// Verify route
+	router.Get("/verify/employe/verifyEmail",EmployeController.VerifyEmail)
+	router.Post("/verify/employe/refreshEmailVerify",EmployeController.RefreshEmailVerify)
+	
+	// Subroute or group Route
+	EmployeRoutes := router.RouteGroup("employes")
+	EmployeRoutes.POST("/signup",EmployeController.RegisterEmploye)
+	EmployeRoutes.POST("/signin",EmployeController.LoginEmploye)
+	EmployeRoutes.GET("/:id",EmployeController.GetEmployeById)
+	
+	router.ErrorHandler() // Middleware error handling
+	
+	router.RouterLogger() // Router Logging
+	router.StartServer(":3000") // Start server
 }
